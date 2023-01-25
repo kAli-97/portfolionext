93f8d16128562f59cc231e0c2daaa0c3f1a3cb00
ali <atemurtas97@gmail.com>
2023-01-25 07:09:43 +0300

    atemurtas/25.01.2023

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,10 +7,22 @@ import Atemurtas from "../components/Atemurtas";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
+type Props = {};
+export default function Home({}: Props) {
+  const [mot, setMot] = useState(null);
+  useEffect(() => {
+    const getData = () => {
+      axios.get("https://type.fit/api/quotes").then((res) => {
+        setMot(res.data);
+      });
+    };
+    getData();
+  }, []);
 
-export default function Home() {
   return (
     <div className="bg-[#000000] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
@@ -28,6 +40,7 @@ export default function Home() {
       </section>
       <section id="footer" className="snap-end">
         <Footer />
+        {mot ? mot[Math.floor(Math.random() * 1643) + 1]?.text : ""}
       </section>
     </div>
   );
